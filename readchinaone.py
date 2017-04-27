e5e8750edbd0843ff9a37c25e7544bf401999780
QLWeilcf <1226824818@qq.com>
2017-04-27 23:03:34 +0800

    Create readchinaone.py
    a simple GIS hw

---
+++ readchinaone.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+def readgisone():
+    import re
+    sfile="D:***.e00"
+    savef="D:\***\chinahadre.txt"
+    lsd=[]
+    fi=open(savef,'a+')
+    with open(sfile,'r') as f:
+        ls=f.readlines()
+    
+    #patz=re.compile(r'E\+')
+    for i in ls:
+        if 'E+' in i:
+            k=i.split()#字符串列表
+            
+
+readgisone()
